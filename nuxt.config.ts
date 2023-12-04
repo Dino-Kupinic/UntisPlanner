3c3f32d91957c0e1c234d468e99bab6ab5e8057e
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-12-04 15:10:23 +0100

    added libraries

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: {
+    enabled: true,
+  },
+  ssr: false,
+  modules: [
+    "@vueuse/nuxt",
+    "@vite-pwa/nuxt",
+    "@nuxt/ui",
+    "nuxt-vitest",
+    "@samk-dev/nuxt-vcalendar",
+  ],
+  pwa: {
+    /* PWA options */
+  },
 })
