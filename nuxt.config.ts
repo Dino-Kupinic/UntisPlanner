9687c4b15a15fabfe537dc1a811b8b09bf4f8661
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-12-05 14:03:50 +0100

    working on pwa offline support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,9 +7,22 @@ export default defineNuxtConfig({
   },
   vue: {
     defineModel: true,
-    propsDestructure: true
+    propsDestructure: true,
   },
   ssr: false,
+  nitro: {
+    esbuild: {
+      options: {
+        target: "esnext",
+      },
+    },
+    prerender: {
+      routes: ["/"],
+    },
+  },
+  imports: {
+    autoImport: true,
+  },
   modules: [
     "@vueuse/nuxt",
     "@vite-pwa/nuxt",
@@ -17,31 +30,35 @@ export default defineNuxtConfig({
     "nuxt-vitest",
     "@samk-dev/nuxt-vcalendar",
     "@pinia/nuxt",
-    ...(isDevelopment) ? [] : ['nuxt-security'],
+    ...(isDevelopment) ? [] : ["nuxt-security"],
   ],
   sourcemap: isDevelopment,
   pwa: {
     manifest: {
       name: "UntisPlanner",
       description: "Intuitive Untis Planner",
-      theme_color: '#ffffff',
+      theme_color: "#ffffff",
       icons: [
         {
-          src: 'pwa-192x192.png',
-          sizes: '192x192',
-          type: 'image/png',
+          src: "pwa-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
         },
         {
-          src: 'pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
         },
         {
-          src: 'pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
         },
       ],
+    },
+    registerType: "autoUpdate",
+    workbox: {
+      navigateFallback: "/",
     },
   },
 })
