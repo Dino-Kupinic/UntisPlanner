e43c128eb4fa36b869ac6403419de2881256fc6f
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-12-05 11:49:16 +0100

    added more config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import {isDevelopment} from "std-env"
+
 export default defineNuxtConfig({
   devtools: {
     enabled: true,
+  },
+  vue: {
+    defineModel: true,
+    propsDestructure: true
   },
   ssr: false,
   modules: [
@@ -11,8 +17,31 @@ export default defineNuxtConfig({
     "nuxt-vitest",
     "@samk-dev/nuxt-vcalendar",
     "@pinia/nuxt",
+    ...(isDevelopment) ? [] : ['nuxt-security'],
   ],
+  sourcemap: isDevelopment,
   pwa: {
-    /* PWA options */
+    manifest: {
+      name: "UntisPlanner",
+      description: "Intuitive Untis Planner",
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
   },
 })
