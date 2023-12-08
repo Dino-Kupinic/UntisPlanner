b12882ec57774fded54487ece865ee60101513a7
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-12-08 18:54:45 +0100

    added a bit of styling

--- components/nav/Navbar.vue
+++ components/nav/Navbar.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <UContainer>
-    <NavBarThemeToggle/>
+  <UContainer class="flex align-center w-full h-16 border-b border-neutral-300 dark:bg-gray-900 dark:border-gray-800">
+    <NavBarThemeToggle />
   </UContainer>
 </template>
