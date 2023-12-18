aaa816b6417080cda804f7b1ed6f5f0eeee71ea1
mploierMacBook <mploier@htl-steyr.ac.at>
2023-12-18 09:33:58 +0100

    Added GithubIcon.vue for GitHub-SVG icon Working on Footer.vue

--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -3,6 +3,21 @@
 </script>
 
 <template>
+  <footer>
+    <div class="flex justify-around max-sm:flex-col max-sm:flex-wrap max-sm:content-center">
+      <div>
+        <p>First Column</p>
+        <GithubIcon />
+      </div>
 
+      <div>
+        <p>Second Column</p>
+      </div>
+
+      <div>
+        <p>Third Column</p>
+      </div>
+    </div>
+  </footer>
 </template>
 
