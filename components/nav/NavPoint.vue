1cd398894ece4ad91ab62cdd024ac54953726430
dsamhabe <dsamhabe@htl-steyr.ac.at>
2023-12-18 09:39:40 +0100

    work on navbar

---
+++ components/nav/NavPoint.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{
+  icon: string
+  text: string
+  path: string
+}>()
+</script>
+
+<template>
+  <UIcon :name="props.icon" />
+  <span>{{props.text}}</span>
+</template>
