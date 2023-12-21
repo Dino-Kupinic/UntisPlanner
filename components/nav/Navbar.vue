5e2b0878a1bea60f02d86085498b0aeeb299520f
dsamhabe <dsamhabe@htl-steyr.ac.at>
2023-12-21 10:14:24 +0100

    small changes

--- components/nav/Navbar.vue
+++ components/nav/Navbar.vue
@@ -3,9 +3,12 @@
 </script>
 
 <template>
-  <UContainer class="flex space-x-40">
-    <UContainer>
+  <UContainer class="flex">
+    <UContainer class="flex-1">
       <NavPoint size="xl" icon="i-heroicons-calendar-days" label="Untis Planner" path="https://htl-steyr.ac.at"></NavPoint>
+    </UContainer>
+    <UContainer>
+
     </UContainer>
     <UContainer>
       <NavPoint icon="i-heroicons-cog-6-tooth" label="Settings" path="/settings"></NavPoint>
