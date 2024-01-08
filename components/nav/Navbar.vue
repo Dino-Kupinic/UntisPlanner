df6ceb3091ec8305a88dab9ee7ca07db49d9a3d9
dsamhabe <dsamhabe@htl-steyr.ac.at>
2024-01-08 17:25:07 +0100

    Navbar is now fully responsive with working burgermenu

--- components/nav/Navbar.vue
+++ components/nav/Navbar.vue
@@ -8,10 +8,12 @@ import NavBurgerMenu from "~/components/nav/NavBurgerMenu.vue";
     <UContainer class="flex-1">
       <NavPoint size="3xl" icon="i-heroicons-calendar-days" label="Untis Planner" path="/"></NavPoint>
     </UContainer>
-    <UContainer class="max-md space-x-5 md:hidden">
+    <UContainer class="space-x-5 max-[775px]:hidden">
       <NavBarThemeToggle class="self-center"/>
       <NavPoint icon="i-heroicons-cog-6-tooth" label="Settings" path="/settings"></NavPoint>
       <NavPoint icon="i-heroicons-question-mark-circle-20-solid" label="Info"></NavPoint>
+    </UContainer>
+    <UContainer class="space-x-5 min-[775px]:hidden">
       <NavBurgerMenu></NavBurgerMenu>
     </UContainer>
   </UContainer>
