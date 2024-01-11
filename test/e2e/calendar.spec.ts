94154480f40997d30432a3b53843fcbbab888d7b
Neuery17Alt <jangerer@htbla-steyr.ac.at>
2024-01-11 14:20:05 +0100

    new e2e tests for Calendar.vue component

---
+++ test/e2e/calendar.spec.ts
@@ -0,0 +1,9 @@
+import {test, expect} from "@playwright/test"
+
+test("Calendar", async ({page}) => {
+  await page.goto('http://localhost:3001/');
+  await page.getByRole('button', { name: 'Februar 2033' }).click();
+  await page.getByLabel('März', { exact: true }).click();
+  await page.locator('button:nth-child(2)').click();
+  await page.getByLabel('Sonntag, 5. Nov.').nth(1).click();
+})
