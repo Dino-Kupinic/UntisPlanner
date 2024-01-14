21941a5a9bf36e96bd02bd979371e8413a95a7e7
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-01-14 01:41:48 +0100

    removed pwa for the time being

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,16 +41,16 @@ export default defineNuxtConfig({
   devServer: {
     port: 3001,
   },
-  nitro: {
-    esbuild: {
-      options: {
-        target: "esnext",
-      },
-    },
-    prerender: {
-      routes: ["/"],
-    },
-  },
+  // nitro: {
+  //   esbuild: {
+  //     options: {
+  //       target: "esnext",
+  //     },
+  //   },
+  //   prerender: {
+  //     routes: ["/"],
+  //   },
+  // },
   colorMode: {
     classSuffix: "",
     preference: "system",
@@ -74,35 +74,35 @@ export default defineNuxtConfig({
     storesDirs: ["./stores/**"],
   },
   sourcemap: isDevelopment,
-  pwa: {
-    manifest: {
-      name: "UntisPlanner",
-      description: "Intuitive Untis Planner",
-      theme_color: "#ffffff",
-      icons: [
-        {
-          src: "untisplanner-icon-192.png",
-          sizes: "192x192",
-          type: "image/png",
-        },
-        {
-          src: "untisplanner-icon-512.png",
-          sizes: "512x512",
-          type: "image/png",
-        },
-      ],
-    },
-    registerType: "autoUpdate",
-    workbox: {
-      navigateFallback: "/",
-    },
-    devOptions: {
-      enabled: true,
-      suppressWarnings: true,
-      navigateFallbackAllowlist: [/^\/$/],
-      type: "module",
-    },
-  },
+  // pwa: {
+  //   manifest: {
+  //     name: "UntisPlanner",
+  //     description: "Intuitive Untis Planner",
+  //     theme_color: "#ffffff",
+  //     icons: [
+  //       {
+  //         src: "untisplanner-icon-192.png",
+  //         sizes: "192x192",
+  //         type: "image/png",
+  //       },
+  //       {
+  //         src: "untisplanner-icon-512.png",
+  //         sizes: "512x512",
+  //         type: "image/png",
+  //       },
+  //     ],
+  //   },
+  //   registerType: "autoUpdate",
+  //   workbox: {
+  //     navigateFallback: "/",
+  //   },
+  //   devOptions: {
+  //     enabled: true,
+  //     suppressWarnings: true,
+  //     navigateFallbackAllowlist: [/^\/$/],
+  //     type: "module",
+  //   },
+  // },
   typescript: {
     typeCheck: true,
     strict: true,
