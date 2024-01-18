344ebc41f2855cf791c0763947a40cc3817335e2
Neuery17Alt <jangerer@htbla-steyr.ac.at>
2024-01-18 08:40:23 +0100

    working on pr review

--- test/e2e/calendar.spec.ts
+++ test/e2e/calendar.spec.ts
@@ -1,9 +1,10 @@
 import {test, expect} from "@playwright/test"
 
 test("Calendar", async ({page}) => {
-  await page.goto('http://localhost:3001/');
-  await page.getByRole('button', { name: 'Februar 2033' }).click();
-  await page.getByLabel('März', { exact: true }).click();
-  await page.locator('button:nth-child(2)').click();
-  await page.getByLabel('Sonntag, 5. Nov.').nth(1).click();
+  await page.goto('/');
+  // TODO
+  // await page.getByRole('button', { name: 'Februar 2033' }).click();
+  // await page.getByLabel('März', { exact: true }).click();
+  // await page.locator('button:nth-child(2)').click();
+  // await page.getByLabel('Sonntag, 5. Nov.').nth(1).click();
 })
