9a79150d2ff0dcf9b038e1cec6e7e3ec87035176
Neuery17Alt <jangerer@htbla-steyr.ac.at>
2024-02-07 14:41:31 +0100

    small changes regarding the generator settings

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 
+import GeneratorSettings from "~/components/generator_settings/GeneratorSettings.vue"
 </script>
 
 <template>
+  <GeneratorSettings></GeneratorSettings>
   <Calendar />
   <ExportTable />
   <ExportTableButton />
