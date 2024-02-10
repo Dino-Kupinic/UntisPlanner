933fdd2d8a3f1bb20a5beae133236690cd5fc998
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-02-10 15:53:12 +0100

    add vercel speed insights

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-
+import {SpeedInsights} from "@vercel/speed-insights/vue"
 </script>
 
 <template>
+  <SpeedInsights/>
   <NavBar/>
   <slot></slot>
   <Footer class="mt-40"/>
