2f2c3d9303f2f368b6605ce6689077d2fb31ff0c
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-02-10 15:13:16 +0100

    wip

--- components/generator_settings/GeneratorSettings.vue
+++ components/generator_settings/GeneratorSettings.vue
@@ -38,29 +38,29 @@ const query = ref<string>("")
 
 <template>
   <div
-    class="m-auto flex flex-wrap place-content-center justify-center w-3/4 sm:w-5/12 sm:h-auto sm:min-h-28 dark:bg-gray-900 border h-1/3 mt-5 p-3 rounded-lg border-neutral-300 dark:border-gray-700"
+    class="m-auto flex flex-wrap justify-evenly items-center w-[260px] sm:w-[1034px] md:w-3/4 lg:w-[1034px] sm:h-auto sm:min-h-28 dark:bg-gray-900 border h-[400px] mt-5 p-3 rounded-lg border-neutral-300 dark:border-gray-700"
   >
-    <div class="ml-2 mr-2 ">
+    <div class="ml-2 mr-2 mt-2 sm:mt-0 w-full sm:w-48">
       <p>Federal State</p>
-      <USelectMenu class="w-[15rem] mt-1" v-model="selectedFederalState" :options="federalStates"
+      <USelectMenu class="sm:w-auto w-full mt-1" v-model="selectedFederalState" :options="federalStates"
                    placeholder="Select federal state"/>
     </div>
-    <div class="ml-2 mr-2 mt-2 sm:mt-0">
+    <div class="ml-2 mr-2 mt-2 sm:mt-0 w-full sm:w-24">
       <p>Period</p>
-      <UInput class="mt-1 w-[15rem]" placeholder="1"></UInput>
+      <UInput class="mt-1 sm:w-auto w-full" placeholder="1"></UInput>
     </div>
-    <div class="ml-2 mr-2 mt-2  sm:mt-0">
+    <div class="ml-2 mr-2 mt-2 sm:mt-0 w-full sm:w-48">
       <p>Lession-Days</p>
-      <USelectMenu class="w-[15rem] mt-1" v-model="selectedWeekday" :options="weekdays" multiple
+      <USelectMenu class="sm:w-auto w-full mt-1" v-model="selectedWeekday" :options="weekdays" multiple
                    placeholder="Select Days">
         <template #label>
           <span v-if="selectedWeekday.length" class="truncate">{{ selectedWeekday.join(", ") }}</span>
         </template>
       </USelectMenu>
     </div>
-    <div class="ml-2 mr-2 mt-2 sm:mt-0">
+    <div class="ml-2 mr-2 mt-2 sm:mt-0 w-full sm:w-auto">
       <p>Teacher</p>
-      <USelectMenu class="w-[15rem] mt-1"
+      <USelectMenu class="sm:w-auto w-full mt-1"
                    v-model="selectedTeacher"
                    v-model:query="query"
                    :options="teachers"
