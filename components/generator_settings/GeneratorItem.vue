89ccd3da09516f7e1b051003b0e9925f76056362
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-02-11 21:25:49 +0100

    wip responsive

--- components/generator_settings/GeneratorItem.vue
+++ components/generator_settings/GeneratorItem.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-
+defineProps<{
+  label: string
+}>()
 </script>
 
 <template>
   <div class="ml-2 mr-2 mt-2 sm:mt-0 w-full lg:w-48 md:w-36 sm:w-24">
+    <p class="text-sm md:text-base">{{ label }}</p>
     <slot/>
   </div>
 </template>
--- components/generator_settings/GeneratorSettings.vue
+++ components/generator_settings/GeneratorSettings.vue
@@ -40,17 +40,14 @@ const query = ref<string>("")
   <div
     class="m-auto flex flex-wrap justify-evenly items-center w-[260px] sm:w-[520px] md:w-[775px] lg:w-[1034px] sm:h-auto sm:min-h-28 dark:bg-gray-900 border h-[400px] mt-5 p-3 rounded-lg border-neutral-300 dark:border-gray-700"
   >
-    <GeneratorItem>
-      <p>Federal State</p>
+    <GeneratorItem label="Federal State">
       <USelectMenu class="sm:w-auto w-full mt-1" v-model="selectedFederalState" :options="federalStates"
                    placeholder="Select federal state"/>
     </GeneratorItem>
-    <GeneratorItem>
-      <p>Period</p>
+    <GeneratorItem label="Period">
       <UInput class="mt-1 sm:w-auto w-full" placeholder="1"></UInput>
     </GeneratorItem>
-    <GeneratorItem>
-      <p>Lession-Days</p>
+    <GeneratorItem label="Lesson Days">
       <USelectMenu class="sm:w-auto w-full mt-1" v-model="selectedWeekday" :options="weekdays" multiple
                    placeholder="Select Days">
         <template #label>
@@ -58,8 +55,7 @@ const query = ref<string>("")
         </template>
       </USelectMenu>
     </GeneratorItem>
-    <GeneratorItem>
-      <p>Teacher</p>
+    <GeneratorItem label="Teacher">
       <USelectMenu class="sm:w-auto w-full mt-1"
                    v-model="selectedTeacher"
                    v-model:query="query"
